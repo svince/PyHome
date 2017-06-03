92adffb31e3eea1178f3cc7b65c434e4561a0c40
svince <svincent@student.ethz.ch>
2017-06-03 16:09:31 +0000

    working prototype for bustimes

--- BusTimes/main.py
+++ BusTimes/main.py
@@ -2,12 +2,45 @@ import sys
 import PyQt5
 from PyQt5.QtWidgets import *
 import mainwindow_auto
+import urllib.request
+import json
 
 class MainWindow(QMainWindow, mainwindow_auto.Ui_MainWindow):
 
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
+        self.updateBusTimes()
+
+    def updateBusTimes(self):
+        times = getStationTimes("thuijastrasse", 5)
+        text = ""
+        for time in times:
+            text += time[0] + " " + time[1] + " " + time[2] + "\n"
+        self.label.setText(text)
+
+def getStationTimes(station_name, limit):
+    request = "http://transport.opendata.ch/v1/stationboard?station="
+    request += station_name + "&limit=" + str(limit)
+    response_bytes = urllib.request.urlopen(request).read()
+    response_json = response_bytes.decode("utf-8")
+    response = json.loads(response_json)
+    board = response["stationboard"]
+    times = []
+    for entry in board:
+        name = entry["name"]
+        to = entry["to"]
+        stop = entry["stop"]
+        departure = stop["departure"]
+        departure_time_pre = departure.split("T")[1]
+        departure_time = departure_time_pre.split("+")[0]
+        times.append([name, to, departure_time])
+    return times      
+
+def test_sbb_access():
+    times = getStationTimes("thujastrasse", 5)
+    for time in times:
+        print(time[0] + " " + time[1] + " " + time[2])
 
 def main():
     app = QApplication(sys.argv)
@@ -17,3 +50,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+    #test_sbb_access()
+        
--- BusTimes/mainwindow_auto.py
+++ BusTimes/mainwindow_auto.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'mainwindow.ui'
 #
-# Created: Fri Jun  2 19:30:47 2017
+# Created: Sat Jun  3 15:49:54 2017
 #      by: PyQt5 UI code generator 5.3.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -15,12 +15,9 @@ class Ui_MainWindow(object):
         MainWindow.resize(400, 300)
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
-        self.listView = QtWidgets.QListView(self.centralWidget)
-        self.listView.setGeometry(QtCore.QRect(70, 20, 256, 192))
-        self.listView.setObjectName("listView")
-        self.textBrowser = QtWidgets.QTextBrowser(self.centralWidget)
-        self.textBrowser.setGeometry(QtCore.QRect(120, 100, 141, 61))
-        self.textBrowser.setObjectName("textBrowser")
+        self.label = QtWidgets.QLabel(self.centralWidget)
+        self.label.setGeometry(QtCore.QRect(27, 10, 341, 221))
+        self.label.setObjectName("label")
         MainWindow.setCentralWidget(self.centralWidget)
         self.menuBar = QtWidgets.QMenuBar(MainWindow)
         self.menuBar.setGeometry(QtCore.QRect(0, 0, 400, 27))
@@ -42,10 +39,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'Roboto\'; font-size:12pt; font-weight:200; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">gaylord</p></body></html>"))
+        self.label.setText(_translate("MainWindow", "Default"))
         self.menuAsd.setTitle(_translate("MainWindow", "asd"))
 
